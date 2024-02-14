df959142ae062f25b0eeb6a1dafa63788c51c6eb
stdog210420 <lin23013@byui.edu>
2024-02-14 21:40:37 +0800

    added to my homepage

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,25 @@
+const list = document.querySelector('#list');
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    if(input.value != '');
+  
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+  
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+  
+    deleteButton.addEventListener('click', () => {
+      list.removeChild(li);
+      input.focus();
+    });
+  
+    input.focus();
+    input.value = ''
+  });
+
+  
