b42f7a52b4c0d8ae9e1f98f82460aed1e3d12bff
stdog210420 <lin23013@byui.edu>
2024-03-09 16:16:08 +0800

    added to my homepage

--- chamber/scripts/member.js
+++ chamber/scripts/member.js
@@ -1,3 +1,13 @@
-const linksURL = "https://stdog210420.github.io/wdd230/data/links.json";
+const linksURL = "https://stdog210420.github.io/wdd230/chamber/data/members.json";
 
 const cards = document.querySelector('.card');
+
+async function getMemberData() {
+    //Store the response from the fetch() method in a const variable named "response"
+    const response = await fetch(url); 
+    //Convert the response to a JSON object using the .json method and store the results in a const variable named "data".
+    const data  = await response.json();
+    //Add a console.table() expression method to check the data response at this point in the console window.
+    // displayMembers(data.members); // note that we reference the prophets array of the JSON data object, not just the object
+    console.table(data.members);// temporary testing of data retreival
+}  
