e33b77625681ae1f3c2c1f52b95339da1970a204
stdog210420 <lin23013@byui.edu>
2024-03-13 22:36:23 +0800

    added to my homepage

--- chamber/scripts/mode.js
+++ chamber/scripts/mode.js
@@ -2,16 +2,16 @@ const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 const content = document.querySelector(".content");
 const main = document.querySelector("main");
-const ulElement = document.querySelector("ul");
-const liElement = document.querySelector("li");
+// const ulElement = document.querySelector("ul");
+// const liElement = document.querySelector("li");
 
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
 		body.style.background = "#fff";
 		content.style.color = "#3C0211";
-		ulElement.style.color = "#3C0211";
-		liElement.style.color = "#3C0211";
+		// ulElement.style.color = "#3C0211";
+		// liElement.style.color = "#3C0211";
 
 		// main.style.background = "#fff";
 		// main.style.color = "#3C0211";
@@ -19,8 +19,8 @@ modeButton.addEventListener("click", () => {
 	} else {
 		body.style.background = "#1F4736";
 		content.style.color = "#FEECED";
-		ulElement.style.color = "#FEECED";
-		liElement.style.color = "#FEECED";
+		// ulElement.style.color = "#FEECED";
+		// liElement.style.color = "#FEECED";
 
 		// main.style.background = "#1F4736";
 		// main.style.color = "#FEECED";
