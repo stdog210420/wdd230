259fab4aecc006bd4076771b7c2dbf45455187eb
stdog210420 <lin23013@byui.edu>
2024-03-16 00:06:52 +0800

    added to my homepage

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const today = Math.floor(new Date().getDay()); // 0 for Sunday, 1 for Monday, ..., 6 for Saturday
+    let today = Math.floor(new Date().getDay()); // 0 for Sunday, 1 for Monday, ..., 6 for Saturday
     const banner = document.getElementById("banner");
     const closeBannerBtn = document.getElementById("closeBanner");
     console.log(today);
-    today = 2;
+    // today = 2;
    // Check if today is Monday, Tuesday, or Wednesday
     if ( today >=1 && today <= 3) { // Monday, Tuesday, Wednesday
         // Close the banner when the close button is clicked
