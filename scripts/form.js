2127e622e39540455527e8e3bb689aef1785ce19
stdog210420 <lin23013@byui.edu>
2024-03-20 21:37:11 +0800

    added to my homepage

--- scripts/form.js
+++ scripts/form.js
@@ -1,13 +1,35 @@
 const kp1 = document.querySelector("#keyphrase");
 const kp2 = document.querySelector("#keyphrase2");
-const message = document.querySelector("#message");
+const message1 = document.querySelector("#message1");
+const message2 = document.querySelector("#message2");
+kp1.addEventListener("input", function(){
+	const kp1_value = kp1.value.trim();
+	const kp1_pattern = /^[a-zA-Z0-9]{8,}$/;
+	if (kp1_pattern.test(kp1_value)){
+        // If the value matches the pattern, it's valid
+		// console.log("Input is valid.");
+		kp1.style.backgroundColor = "#fff";
+		kp1.style.color = "#000";
+		message1.style.display = "none";
+	}
+	else{
+		// console.log("Input is invalid.");
+		message1.textContent = "❗Please revise to conform with the format.";
+		message1.style.visibility = "show";
+		kp1.style.backgroundColor = "#fff0f3";
+		kp1_value = "";
+		kp1.focus();
+	}
+});
+
+
 kp2.addEventListener("focusout", checkSame);
 
 // This should be refactored.
 function checkSame() {
 	if (kp1.value !== kp2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
-		message.style.visibility = "show";
+		message2.textContent = "❗Key Phrases DO NOT MATCH!";
+		message2.style.visibility = "show";
 		kp2.style.backgroundColor = "#fff0f3";
 		kp2.value = "";
 		kp2.focus();
