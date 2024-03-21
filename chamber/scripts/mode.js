528e3d5a8f1613d026c2cdeb804ce43273a5287e
stdog210420 <lin23013@byui.edu>
2024-03-21 19:45:09 +0800

    added to my homepage

--- chamber/scripts/mode.js
+++ chamber/scripts/mode.js
@@ -1,29 +1,21 @@
 const modeButton = document.querySelector("#mode");
-const body = document.querySelector("body");
-const content = document.querySelector(".content");
-const main = document.querySelector("main");
-// const ulElement = document.querySelector("ul");
-// const liElement = document.querySelector("li");
+const dark = document.querySelector(".dark");
+
 
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
-		body.style.background = "#fff";
-		content.style.color = "#3C0211";
-		// ulElement.style.color = "#3C0211";
-		// liElement.style.color = "#3C0211";
+		dark.style.background = "#fff";
+		dark.style.color = "#3C0211";
 
-		// main.style.background = "#fff";
-		// main.style.color = "#3C0211";
+		// modeButton.classList.toggle("dark");
 		modeButton.textContent = "🔆";
 	} else {
-		body.style.background = "#1F4736";
-		content.style.color = "#FEECED";
-		// ulElement.style.color = "#FEECED";
-		// liElement.style.color = "#FEECED";
+		dark.style.background = "#1F4736";
+		dark.style.color = "#FEECED";
 
-		// main.style.background = "#1F4736";
-		// main.style.color = "#FEECED";
 		modeButton.textContent = "🕶️";
 	}
 });
+
+
