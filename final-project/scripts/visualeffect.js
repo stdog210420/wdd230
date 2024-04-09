f95078040e7ef1931d2138314c1cc2e0366956c7
stdog210420 <lin23013@byui.edu>
2024-04-10 01:54:58 +0800

    added to my homepage

--- final-project/scripts/visualeffect.js
+++ final-project/scripts/visualeffect.js
@@ -1,7 +1,7 @@
 window.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const parallaxElement = document.querySelector('.hero-box');
-    const content = document.querySelector('.content');
+    // const content = document.querySelector('.content');
     parallaxElement.style.transform = `translateY(-${scrollPosition * 0.5}px)`;
     // const rentalImg = document.querySelector('.rental-img');
     // rentalImg.style.transform = `translateY(-${scrollPosition * 0.5}px)`;
